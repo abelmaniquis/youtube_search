c15c5a5e8d21399b9798e30741d8b8944c1ddaf9
abelmaniquis <abelmaniquis@gmail.com>
2016-05-10 01:29:15 +0000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// https://developers.google.com/youtube/v3/docs/activities#properties
+//AIzaSyA_UbnYSWN9BafPa_N_y6WGe4P5J98X0wc
+
+
+$(function(){
+    $("button").click(function(){
+       $("ul").append("<li>testInput</li>") 
+    });
+});
+
+/*
+when search button is clicked, 
+it should find matching results on youtube.com 
+and append them to the ul in the search-results div id
+
+
+*/
